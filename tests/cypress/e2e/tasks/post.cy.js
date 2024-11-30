23977b7537170d655ef2a7d9aa5c0df231fb38c9
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-11-30 19:12:46 -0300

    feat: verificando a mensagem recebida na fila de email

--- tests/cypress/e2e/tasks/post.cy.js
+++ tests/cypress/e2e/tasks/post.cy.js
@@ -6,26 +6,50 @@ describe('POST /tasks', () => {
     })
   });
 
-  it('register a new task', function () {
-    const { user, task } = this.tasks.create
-
-    cy.task('removeUser', user.email)
-    cy.postUser(user)
-
-    cy.postSession(user).then(userResp => {
-
-      cy.task('removeTask', task.name, user.email)
-
-      cy.postTask(task, userResp.body.token).then(response => {
-        expect(response.status).to.eq(201)
-        expect(response.body.name).to.eq(task.name)
-        expect(response.body.tags).to.eql(task.tags)
-        expect(response.body.is_done).to.be.false
-        expect(response.body.user).to.eql(userResp.body.user._id)
-        expect(response.body._id.length).to.eql(24)
+  context('register a new task', function () {
+    before(function () {
+      cy.purgeQueueMessages().then(response => {
+        expect(response.status).to.eq(204)
       })
     })
-  });
+
+    it('post task', function () {
+      const { user, task } = this.tasks.create
+
+      cy.task('removeUser', user.email)
+      cy.postUser(user)
+
+      cy.postSession(user).then(userResp => {
+
+        cy.task('removeTask', task.name, user.email)
+
+        cy.postTask(task, userResp.body.token).then(response => {
+          expect(response.status).to.eq(201)
+          expect(response.body.name).to.eq(task.name)
+          expect(response.body.tags).to.eql(task.tags)
+          expect(response.body.is_done).to.be.false
+          expect(response.body.user).to.eql(userResp.body.user._id)
+          expect(response.body._id.length).to.eql(24)
+        })
+      })
+    });
+
+    after(function () {
+      const { user, task } = this.tasks.create
+      
+      cy.wait(1000)
+      cy.getMessageQueue().then(response => {
+        expect(response.status).to.eq(200)
+
+        cy.log(JSON.stringify(response.body))
+        expect(response.body[0].payload).to.include(user.name.split(' ')[0])
+        expect(response.body[0].payload).to.include(task.name)
+        expect(response.body[0].payload).to.include(user.email)
+      })
+    })
+  })
+
+
 
   it('duplicate tasks', function () {
     const { user, task } = this.tasks.dup
@@ -40,8 +64,47 @@ describe('POST /tasks', () => {
       cy.postTask(task, userResp.body.token)
       cy.postTask(task, userResp.body.token).then(response => {
         expect(response.status).to.eq(409)
-        expect(response.body.message).to.eq("Duplicated task!")
+        expect(response.body.message).to.eq('Duplicated task!')
       })
     })
   });
+});
+
+Cypress.Commands.add('purgeQueueMessages', () => {
+  cy.api({
+    url: 'https://jackal.rmq.cloudamqp.com/api/queues/lctcobse/tasks/contents',
+    method: 'DELETE',
+    body: {
+      vhost: 'lctcobse',
+      name: 'tasks',
+      mode: 'purge'
+    },
+    headers: {
+      Authorization: 'Basic bGN0Y29ic2U6clJQMWFWT2JkR3J0Z2FBcEl2RVhxTHNNeHgyajlUQ2c='
+    },
+    failOnStatusCode: false
+  }).then(response => {
+    return response
+  })
+});
+
+Cypress.Commands.add('getMessageQueue', () => {
+  cy.api({
+    url: 'https://jackal.rmq.cloudamqp.com/api/queues/lctcobse/tasks/get',
+    method: 'POST',
+    body: {
+      vhost: 'lctcobse',
+      name: 'tasks',
+      truncate: '50000',
+      ackmode: 'ack_requeue_true',
+      encoding: 'auto',
+      count: '1'
+    },
+    headers: {
+      Authorization: 'Basic bGN0Y29ic2U6clJQMWFWT2JkR3J0Z2FBcEl2RVhxTHNNeHgyajlUQ2c='
+    },
+    failOnStatusCode: false
+  }).then(response => {
+    return response
+  })
 });
